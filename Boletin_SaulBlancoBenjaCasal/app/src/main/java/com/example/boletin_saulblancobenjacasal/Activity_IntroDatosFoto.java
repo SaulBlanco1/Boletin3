5f43659359c81b131fe4664b56b53c7608ceba2a
SaulBlanco1 <118265909+SaulBlanco1@users.noreply.github.com>
2022-11-16 12:14:34 +0100

    cosas varias

--- Boletin_SaulBlancoBenjaCasal/app/src/main/java/com/example/boletin_saulblancobenjacasal/Activity_IntroDatosFoto.java
+++ Boletin_SaulBlancoBenjaCasal/app/src/main/java/com/example/boletin_saulblancobenjacasal/Activity_IntroDatosFoto.java
@@ -3,12 +3,38 @@ package com.example.boletin_saulblancobenjacasal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
 
 public class Activity_IntroDatosFoto extends AppCompatActivity {
+
+    TextView textView_IntroNombreFoto;
+    ImageView imageView_imagen;
+    EditText editText_nombreAntiguo;
+    EditText editText_nombreNuevo;
+    EditText editText_IntroNombreFoto;
+    Button button_Crear;
+    Button button_Modificar;
+    Button button_eliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_datos_foto);
+
+        textView_IntroNombreFoto=(TextView) findViewById(R.id.textView_IntroNombreFoto);
+        editText_IntroNombreFoto=(EditText) findViewById(R.id.editText_IntroNombreFoto);
+        editText_nombreAntiguo=(EditText) findViewById(R.id.editText_nombreAntiguo);
+        editText_nombreNuevo=(EditText)findViewById(R.id.editText_nombreNuevo);
+        imageView_imagen=(ImageView) findViewById(R.id.imageView_imagen);
+        button_Crear=(Button) findViewById(R.id.button_Crear);
+        button_Modificar=(Button) findViewById(R.id.button_Modificar);
+        button_eliminar=(Button) findViewById(R.id.button_eliminar);
+
+
     }
 }
--- Boletin_SaulBlancoBenjaCasal/app/src/main/java/com/example/boletin_saulblancobenjacasal/MainActivity.java
+++ Boletin_SaulBlancoBenjaCasal/app/src/main/java/com/example/boletin_saulblancobenjacasal/MainActivity.java
@@ -5,26 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/*
-CRUD
-C --> Create/Crear
-R --> Read/Select
-U --> Update/Actualizar
-D --> Delete/Borrar
 
- */
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_IntroNombre;
+    Button button_Jugar;
+    Button button_VerPuntuaciones;
+    Button button_Administrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_Jugar=(Button) findViewById(R.id.button_Jugar);
+        button_VerPuntuaciones=(Button) findViewById(R.id.button_VerPuntuaciones);
+        button_Administrar=(Button)findViewById(R.id.button_Administrar);
+        editText_IntroNombre=(EditText) findViewById(R.id.editText_IntroNombre);
 
     }
 
     public void administrar(View view){
         Intent intent = new Intent(MainActivity.this, Activity_IntroDatosFoto.class);
+        startActivity(intent);
+    }
+
+    public void puntuaciones(View view){
+        Intent intent = new Intent(MainActivity.this, Acitivity_TablasPuntuacion.class);
         startActivity(intent);
     }
 }
